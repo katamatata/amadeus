5b8a660922b11282df4f5a7cc9fa2c5a0b4f77be
katamatata <51786805+katamatata@users.noreply.github.com>
2020-07-10 15:43:44 +0200

    Add libraries for React Router, styled components and FontAwesome icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import {faHome} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 function App() {
-  return <div className="App">amadeus</div>;
+  return (
+    <div className="App">
+      amadeus
+      <FontAwesomeIcon icon={faHome} />
+    </div>
+  );
 }
 
 export default App;
